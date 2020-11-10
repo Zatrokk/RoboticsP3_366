ed7ac23c9182c28c29addb1a2c9c6544ad1522ea
Jonathan Falk <jfalk19@student.aau.dk>
2020-11-10 15:28:10 +0100

    Added stuff to Local

--- Local/Trying/Trying/Trying.cpp
+++ Local/Trying/Trying/Trying.cpp
@@ -1,15 +1,14 @@
-// Trying.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
+/*#include <iostream>
 
 #include "../Include/dynamixel_sdk/dynamixel_sdk.h"
+
+
 
 int main()
 {
     std::cout << "Hello World!\n";
     return 0;
-}
+}*/
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
